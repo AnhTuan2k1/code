d293226657b5750dcb6456d07d218aec61e96cce
TranTuan <anhtuanat2k1@gmail.com>
2024-04-11 13:45:27 +0700

    up

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class HelloWorld {
@@ -21,11 +22,15 @@ public class HelloWorld {
         };
 
 
-        System.out.println("Hello"); 
-        System.out.println(Sum15(3));
-        System.out.println(Tich10(3, 3));
+        System.out.println("Hello");
+        System.out.println(IncreaseNum60(15570));
+        System.out.println(CountNum43(2101));
+        System.out.println(SumNum44(2101));
+        System.out.println(ProductNum45(2101));
+        System.out.println(CountOddNum46(2101));
+        System.out.println(Power10(3, 3));
         System.out.println(Sum40(3, 3));
-        System.out.println(FindMax90(15));
+        System.out.println(IsPrime31(2));
         System.out.println(ReadNumber105(20));
         DrawTriangle111(5);
         Function120(15);
@@ -119,13 +124,13 @@ public class HelloWorld {
     }
 
     //9.
-    public static float Sum9(int n){
-        if(n>1) return n * Sum9(n-1);
+    public static float Factorial9(int n){
+        if(n>1) return n * Factorial9(n-1);
         else return n;
     }
 
     //10. Tính T(x, n) = x^n
-    public static float Tich10(int x, int n){
+    public static float Power10(int x, int n){
         int s = 1;
         if(n==0) return 1;
         while (n > 0) {
@@ -163,7 +168,7 @@ public class HelloWorld {
     public static float Sum13(int n, int x){
         float s = 0;
         for (int i = 1; i <= n; i++) {
-            s = s + Tich10(x, 2*i);
+            s = s + Power10(x, 2*i);
         }
 
         return s;
@@ -173,7 +178,7 @@ public class HelloWorld {
     public static float Sum14(int n, int x){
         float s = 0;
         for (int i = 0; i <= n; i++) {
-            s = s + Tich10(x, 2*i + 1);
+            s = s + Power10(x, 2*i + 1);
         }
 
         return s;
@@ -195,7 +200,7 @@ public class HelloWorld {
     public static float Sum16(int n, int x){
         float s = 0;
         for (int i = 1; i <= n; i++) {
-            s = s + Tich10(x, i)/Sum1(i);
+            s = s + Power10(x, i)/Sum1(i);
         }
 
         return s;
@@ -205,7 +210,7 @@ public class HelloWorld {
     public static float Sum17(int n, int x){
         float s = 0;
         for (int i = 1; i <= n; i++) {
-            s = s + Tich10(x, i)/Sum9(i);
+            s = s + Power10(x, i)/Factorial9(i);
         }
 
         return s;
@@ -215,7 +220,7 @@ public class HelloWorld {
     public static float Sum18(int n, int x){
         float s = 0;
         for (int i = 1; i <= n; i++) {
-            s = s + Tich10(x, 2*i)/Sum9(2*i);
+            s = s + Power10(x, 2*i)/Factorial9(2*i);
         }
 
         return s;
@@ -225,7 +230,7 @@ public class HelloWorld {
     public static float Sum19(int n, int x){
         float s = 0;
         for (int i = 1; i <= n; i++) {
-            s = s + Tich10(x, 2*i + 1)/Sum9(2*i + 1);
+            s = s + Power10(x, 2*i + 1)/Factorial9(2*i + 1);
         }
 
         return s;
@@ -299,7 +304,7 @@ public class HelloWorld {
     //26. Tính tích tất cả “ước số lẻ" của số nguyên dương n.
     public static float ProductOddDivisor26(int n){
         float s = 1;
-        for (int i = 2; i < n; i+=2) {
+        for (int i = 1; i < n; i+=2) {
             if(n%i==0 && i%2 == 1){
                s*=i;
             }
@@ -318,6 +323,27 @@ public class HelloWorld {
         return count;
     }
 
+    //28. Cho số nguyên dương n. Tính tổng các ước số nhỏ hơn chính nó.
+    public static float SumDivisor28(int n){
+        float sum = 0;
+        for (int i = 1; i < n; i++) {
+            if(n%i==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+
+    //29. Tìm ước số lẻ lớn nhất của số nguyên dương n.
+    public static float FindMaxOddDivisor29(int n){
+        for (int i = n; i < n; i--) {
+            if(n%i==0 && i%2 == 1){
+                return i;
+            }
+        }
+        return 1;
+    }
+
     //30. Cho số nguyên dương n. Kiểm tra số dương n có phải là số hoàn thiện hay không?
     public static boolean IsPerfect30(int n){
         int sumDivisor = 0;
@@ -330,6 +356,44 @@ public class HelloWorld {
         return sumDivisor == n;
     }
 
+    //31. Cho số nguyên dương n. Kiểm tra số nguyên dương n có phải là số nguyên tố hay không?
+    public static boolean IsPrime31(int n){
+        if(n<2) return false;
+
+        for (int i = 2; i < n; i++) {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+
+    //32. Cho số nguyên dương n. Kiểm tra số nguyên dương n có phải là số chính phương hay không?
+    public static boolean IsPerfectsquare32(int n){
+        for (int i = 2; i < n/2+1; i++) {
+            if(i*i==n) return true;
+        }
+        return false;
+    }
+
+    //33
+    public static double Sum33(int n){
+        double s = 0;
+        while (n > 0) {
+            s += Math.sqrt(2);
+            n--;
+        }
+        return s;
+    }
+
+    //34. 
+    public static float Sum34(int n){
+        double s = 0;
+        for (int i = 1; i <= n; i++) {
+            s = Math.sqrt(s + i);
+        }
+
+        return (float) s;
+    }
+
     //35. Tính S(n) = √1 + √2 + √3 + ⋯ √n − 1 + √n có n dấu căn.
     public static double Sum35(int n){
         double s = 0;
@@ -340,6 +404,16 @@ public class HelloWorld {
         return s;
     }
 
+    //36. 
+    public static float Sum36(int n){
+        double s = 0;
+        for (int i = 1; i <= n; i++) {
+            s = Math.sqrt(s + Factorial9(i));
+        }
+
+        return (float) s;
+    }
+
     //40. Tính S(n) = √x
     public static double Sum40(int n, int x){
         double s = 0;
@@ -347,6 +421,360 @@ public class HelloWorld {
             s = Math.sqrt(power + s);
         }
         return s;
+    }
+
+    //41
+    public static float Sum41(int n){
+        float s = 1/2f;
+        if(n == 1) return s;
+        
+        while (n > 1) {
+            s = 1f/(1+s);
+            n--;
+        }
+        return s;
+    }
+
+    //42
+    public static float FindNum42(int n){
+        float s = 0;
+        int i = 0;
+        for(; s<n; i++){
+            s+=i;
+        }
+        return i;
+    }
+
+    //43. Hãy đếm số lượng chữ số của số nguyên dương n.
+    public static int CountNum43(int n){
+        if(n > 9) return 1+CountNum43(n/10);
+        else return 1;
+    }
+
+    //44. Hãy tính tổng các chữ số của số nguyên dương n.
+    public static int SumNum44(int n){
+        if(n > 9) return n%10+SumNum44(n/10);
+        else return n;
+    }
+
+    //45. Hãy tính tích các chữ số của số nguyên dương n.
+    public static int ProductNum45(int n){
+        if(n > 9) return (n%10)*ProductNum45(n/10);
+        else return n;
+    }
+
+    //46. Hãy đếm số lượng chữ số lẻ của số nguyên dương n.
+    public static int CountOddNum46(int n){
+        if(n > 9) return ((n%10)%2==1?1:0)+CountOddNum46(n/10);
+        else return n%2==1?1:0;
+    }
+
+    //47. Hãy tính tổng các chữ số chẵn của số nguyên dương n.
+    public static int SumEvenNum47(int n){
+        int num = n%10;
+        if(n > 9) return (num%2==0?num:0)+SumEvenNum47(n/10);
+        else return num%2==0?num:0;
+    }
+
+    //48. Hãy tính tích các chữ số lẻ của số nguyên dương n.
+    public static int ProductOddNum48(int n){
+        int num = n%10;
+        if(n > 9) return (num%2==1?num:1)*ProductOddNum48(n/10);
+        else return num%2==1?num:1;
+    }
+
+    //49. Cho số nguyên dương n. Hãy tìm chữ số đầu tiên của n.
+    public static int FirstNum49(int n){
+        if(n > 9) return FirstNum49(n/10);
+        else return n;
+    }
+
+    //50. Hãy tìm chữ số đảo ngược của số nguyên dương n.
+    public static int ReverseNum50(int n){
+        int reverse = 0;
+        while (n > 0) {
+            reverse = reverse*10 + n%10;
+            n = n/10;
+        }
+
+        return reverse;
+    }
+
+    //51. Tìm chữ số lớn nhất của số nguyên dương n.
+    public static int BiggestNum51(int n){
+        int big = 0;
+        while (n > 0) {
+            if(n%10 > big) big = n%10;
+            n = n/10;
+        }
+
+        return big;
+    }
+
+    //52. Tìm chữ số nhỏ nhất của số nguyên dương n.
+    public static int SmallestNum52(int n){
+        int small = 9;
+        while (n > 0) {
+            if(n%10 < small) small = n%10;
+            n = n/10;
+        }
+
+        return small;
+    }
+
+    //53. Hãy đếm số lượng chữ số lớn nhất của số nguyên dương n.
+    public static int CountBiggestNum53(int n){
+        int big = 0;
+        int count = 1;
+        while (n > 0) {
+            if(n%10 > big) {
+                big = n%10;
+                count = 1;
+            }
+            else if(n%10 == big) count++;
+
+            n = n/10;
+        }
+
+        return count;
+    }
+
+    //54. Hãy đếm số lượng chữ số nhỏ nhất của số nguyên dương n.
+    public static int CountSmallestNum54(int n){
+        int small = 9;
+        int count = 1;
+        while (n > 0) {
+            if(n%10 < small) {
+                small = n%10;
+                count = 1;
+            }
+            else if(n%10 == small) count++;
+            
+            n = n/10;
+        }
+
+        return count;
+    }
+
+    //55. Hãy đêm số lượng chữ số đầu tiên của số nguyên dương n.
+    public static int CountFirstNum55(int n){
+        int firstNum = FirstNum49(n);
+        int count = 1;
+        while (n > 0) {
+            if(n%10 == firstNum) count++;
+            n = n/10;
+        }
+
+        return count;
+    }
+
+    //56. Hãy kiểm tra số nguyên dương n có toàn chữ số lẻ hay không?
+    public static boolean IsAllOddNum56(int n){
+        while (n > 0) {
+            if((n%10)%2==0) return false;
+            n = n/10;
+        }
+
+        return true;
+    }
+
+    //57. Hãy kiểm tra số nguyên dương n có toàn chữ số chẵn hay không?
+    public static boolean IsAllOddNum57(int n){
+        while (n > 0) {
+            if((n%10)%2==1) return false;
+            n = n/10;
+        }
+
+        return true;
+    }
+
+    //59. Hãy kiểm tra số nguyên dương n có phải số đối xứng hay không?
+    public static boolean SymmetricalNum59(int n){
+        String num = String.valueOf(n);
+
+        for (int i = 0; i < num.length()/2; i++) {
+            if(num.charAt(i) != num.charAt(num.length() - i)) return false;
+        }
+
+        return true;
+    }
+
+    //60. Hãy kiểm tra các chữ số của số nguyên dương n có tăng dần từ trái sang phải hay không?
+    public static boolean IncreaseNum60(int n){
+        int currentNum = 9;
+
+        while (n > 0) {
+            if(n%10 <= currentNum) currentNum = n%10;
+            else return false;
+            n = n/10;
+        }
+
+        return true;
+    }
+
+    //61. Hãy kiểm tra các chữ số của số nguyên dương n có giảm dần từ trái sang phải hay không?
+    public static boolean IncreaseNum61(int n){
+        int currentNum = 0;
+
+        while (n > 0) {
+            if(n%10 >= currentNum) currentNum = n%10;
+            else return false;
+            n = n/10;
+        }
+
+        return true;
+    }
+
+    //62. Giải phương trình ax + b = 0.
+    public static double SolveLinearEquation62(double a, double b) {
+        if (a == 0) System.out.println("ko phai pt bac 1");
+
+        return -b / a;
+    }
+
+    //67
+    public static float Sum67(int n, int x){
+        float s = 0;
+
+        while (n > 0) {
+            s += Power10(x, n) * (n+1)%2==0?1:-1;
+            n--;
+        }
+        return s;
+    }
+
+    //68
+    public static float Sum68(int n, int x){
+        float s = 0;
+
+        while (n > 0) {
+            s += Power10(x, 2*n) * n%2==0?1:-1;
+            n--;
+        }
+        return s;
+    }
+
+    //69
+    public static float Sum69(int n, int x){
+        float s = 0;
+
+        while (n >= 0) {
+            s += Power10(x, 2*n + 1) * n%2==0?1:-1;
+            n--;
+        }
+        return s;
+    }
+
+    //70
+    public static float Sum70(int n){
+        float s = 0;
+
+        while (n >= 0) {
+            s += 1/Sum1(n) * (n+1)%2==0?1:-1;
+            n--;
+        }
+        return s;
+    }
+
+    //71
+    public static float Sum71(int n, int x){
+        float s = 0;
+
+        while (n >= 0) {
+            s += Power10(x, n)/Sum1(n) * n%2==0?1:-1;
+            n--;
+        }
+        return s;
+    }
+
+    //72
+    public static float Sum72(int n, int x){
+        float s = 0;
+
+        while (n >= 0) {
+            s += Power10(x, n)/Factorial9(n) * n%2==0?1:-1;
+            n--;
+        }
+        return s;
+    }
+
+    //73
+    public static float Sum73(int n, int x){
+        float s = 0;
+
+        while (n >= 0) {
+            s += Power10(x, 2*n)/Factorial9(2*n) * (n+1)%2==0?1:-1;
+            n--;
+        }
+        return s;
+    }
+
+    //74
+    public static float Sum74(int n, int x){
+        float s = 0;
+
+        while (n >= 0) {
+            s += Power10(x, 2*n + 1)/Factorial9(2*n + 1) * (n+1)%2==0?1:-1;
+            n--;
+        }
+        return s;
+    }
+
+    //78. Liệt kê tất cả các ước số của số nguyên dương n.
+    public static int[] Divisor78(int n, int x){
+        ArrayList<Integer> a = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if(n%i==0){
+                a.add(i);
+            }
+        }
+
+        return Arrays.stream(a.toArray(new Integer[a.size()]))
+        .mapToInt(Integer::intValue).toArray();
+    }
+
+    //82. Viết chương trình tìm số lớn nhất trong ba số thực a, b, c.
+    public static float Max(int a, int b, int c){
+        if(a>=c && a>=b) return a;
+        else if(b>=a && b>=c) return b;
+        else return c;
+    }
+
+    //83. Viết chương trình nhập vào hai số thực. Kiểm tra xem chúng có cùng dấu hay không.
+    public static boolean SameSign(float a, float b){
+        //return (a+b > a && a+b > b) || (a+b < a && a+b < b);
+        return a*b>0;
+    }
+
+    //85. Nhập vào tháng của một năm. Cho biết tháng thuộc quí mấy trong năm.
+    public static int Month(int month){
+        if(month == 1 ||month == 2 ||month == 3) return 1;
+        else if(month == 4 ||month == 5 ||month == 6) return 2;
+        else if(month == 7 ||month == 8 ||month == 9) return 3;
+        else return 4;
+    }
+
+    //86. Tính S(n) = 13 + 23 + 33 + ... + n3
+    public static int Sum86(int n){
+        int s = 0;
+        while (n > 0) {
+            s+=n*n*n;
+            n--;
+        }
+
+        return s;
+    }
+
+    //87. Tìm số nguyên dương n nhỏ nhất sao cho 1 + 2 + ... + n > 10000.
+    public static int FindMinNum87(){
+        int s = 0;
+        int i = 0;
+        while (s < 1000) {
+            i++;
+            s+=i;
+        }
+        return i;
     }
 
     //90. Viết chương trình tìm số nguyên dương m lớn nhất sao cho 1 + 2 + 3 + ... + m < N.
@@ -362,7 +790,7 @@ public class HelloWorld {
     }
 
     //100.Viết chương trình giải phương trình bậc 2.
-    public static void giaiPhuongTrinhBacHai100(double a, double b, double c) {
+    public static void GiaiPhuongTrinhBacHai100(double a, double b, double c) {
         double delta = b * b - 4 * a * c;
 
         if (delta > 0) {
